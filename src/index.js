929c4c52ec16f8985b76e859fc6650507fcc1112
Exx1 <exx-3@yandex.ru>
2022-07-27 14:25:49 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
-// You should implement your task here.
+// You should implement your task here. module.exports
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  }
+  
+  const arr = [];
+  
+  for (i = 0; i < matrix.length; i++) {
+    if (i%2 !== 0) {
+      arr.push(matrix[i].reverse());
+    } else {
+      arr.push(matrix[i]);
+    }
+  }
+  const arrNew = [].concat(...arr);
+ 
+  
+  
+ return arrNew;
 }
+
+
+
+
+
+
